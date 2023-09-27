b04036ede7ca49c06e7f7809161275393f30ea73
gsingh <gauri.singh@nissan-usa.com>
2023-09-26 18:53:36 -0700

    Added youtube player functionality in a dialog as well

--- app/src/main/java/com/learning/TestMessageMaster/MainActivity.java
+++ app/src/main/java/com/learning/TestMessageMaster/MainActivity.java
@@ -1,12 +1,26 @@
 package com.learning.TestMessageMaster;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 public class MainActivity extends AppCompatActivity {
     Button addMessageButton, viewMessagesButton;
@@ -29,5 +43,48 @@ public class MainActivity extends AppCompatActivity {
             i.putExtra("link",inputLink.getText().toString());
             startActivity(i);
         });
+        findViewById(R.id.viewYTDialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+    }
+    public void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        // Inflate the custom youtube layout
+        View customView = inflater.inflate(R.layout.youtube_dialog, null);
+
+        YouTubePlayerView youTubePlayerView = customView.findViewById(R.id.video_dialog);
+        //getLifecycle().addObserver(youTubePlayerView);
+        // Initialize the YouTube player
+        String finalLinkId = "dQw4w9WgXcQ";
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                // loading the selected video into the YouTube Player
+                youTubePlayer.loadVideo(finalLinkId, 0);
+            }
+
+            @Override
+            public void onStateChange(@NonNull YouTubePlayer youTubePlayer, @NonNull PlayerConstants.PlayerState state) {
+                // this method is called if video has ended,
+                super.onStateChange(youTubePlayer, state);
+            }
+        });
+
+        builder.setView(customView);
+        AlertDialog dialog = builder.create();
+        Window window = dialog.getWindow();
+        if (window != null) {
+            WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+            layoutParams.copyFrom(window.getAttributes());
+            layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+            layoutParams.gravity = Gravity.TOP;
+            window.setAttributes(layoutParams);
+        }
+        dialog.show();
     }
 }
